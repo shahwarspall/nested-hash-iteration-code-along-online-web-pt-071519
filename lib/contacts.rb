b93d5f7efc3e897e07931305408b38b151e57226
Shahwar Spall <shevy.spall@gmail.com>
2019-07-27 04:13:28 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,7 +16,7 @@ require 'pry'
   
 def remove_strawberry(contacts)
 
-  contacts.each do |person, contact_details_hash|
+  contacts.each do |pewrson, contact_details_hash|
     if person == "Freddy Mercury"
       contact_details_hash.each do |attribute, data|
         if attribute == :favorite_ice_cream_flavors
